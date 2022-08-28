0e7b680e4aa5910a7d1e48a3ba2b8c32ac99f54a
Syed Saud Arif <syedsaud.arif@phonepe.com>
2022-08-28 22:03:05 +0530

    Fixed the reference counter problem. Also fixed repeat functionality. Added additional test cases.

--- Sources/SwiftDisplayLink/SwiftDisplayLink.swift
+++ Sources/SwiftDisplayLink/SwiftDisplayLink.swift
@@ -1,21 +1,6 @@
 import Foundation
 import QuartzCore
 
-/// `SwiftDisplayLinkDelegate` is a protocol that is used to send events by `SwiftDisplayLink`
-///
-/// Any object that needs to recieve the events of `SwiftDisplayLink` should implement this protocol.
-///
-public protocol SwiftDisplayLinkDelegate: AnyObject {
-    
-    func eventOccured(frame:Int, duration:CFTimeInterval)
-    func getFrameDuration(_ index:Int) -> CFTimeInterval
-    func getNumberOfFrames() -> Int
-    func constructImage(_ frame:Int)
-    func isImageConstructed(_ frame:Int) -> Bool
-    func queueForDisplayImage(_ frame:Int)
-    //func displayImage(_ frame:Int)
-}
-
 public struct SwiftDisplayLinkFrameData {
     let duration: CFTimeInterval
     let isFrameConstructed: Bool
@@ -37,9 +22,15 @@ public class SwiftDisplayLink {
     private let frameDataBlock: SwiftDisplayLinkFrameDataBlock
     private var eventCallBlock: SwiftDisplayLinkEventBlock?
     private var isPlaying: Bool = false
+    private var frame:Int = 0
+    private var willRepeat: Bool
+    private var nextDuration: CFTimeInterval = minDuration
+    private static let minDuration:CFTimeInterval = 0.016666667
+
     
-    public init( frameCount: Int, _ frameData: @escaping SwiftDisplayLinkFrameDataBlock ) {
+    public init( frameCount: Int, repeatFrames: Bool = false, _ frameData: @escaping SwiftDisplayLinkFrameDataBlock ) {
         frameDataBlock = frameData
+        willRepeat = repeatFrames
         numberOfFrames = frameCount
     }
     
@@ -49,15 +40,33 @@ public class SwiftDisplayLink {
         startEvents()
     }
     
+    public func resume() {
+        setUpFrameStart()
+        startEvents()
+    }
+    
+    public func pause() {
+        stopEvents()
+    }
+    
+    public func invalid() {
+        eventCallBlock = nil
+        SwiftDisplayLinkWrapper.shared.removeEventBlock(for: self)
+    }
+    
     private func setUpFrameStart() {
         SwiftDisplayLinkWrapper.shared.setUpFrameStart()
         SwiftDisplayLinkWrapper.shared.addEventBlock(object: self, getAnimatingBlock())
-        frame = 0
+//        frame = 0
     }
     
     private func startEvents() {
         isPlaying = true
         SwiftDisplayLinkWrapper.shared.start()
+    }
+    
+    private func stopEvents() {
+        isPlaying = false
     }
     
     
@@ -85,36 +94,25 @@ public class SwiftDisplayLink {
     }
     
     
-    private weak var delegate: SwiftDisplayLinkDelegate?
-    
-    
-    private var frame:Int = -1
-    private var nextDuration: CFTimeInterval = minDuration
-    private static let minDuration:CFTimeInterval = 0.016666667
-    
-//    private var numberOfFrames: Int {
-//        if let count = delegate?.getNumberOfFrames() {
-//            return count
-//        }
-//        return 0
-//    }
-    
-    
 
-
-    
-    
-    
     
     private func setupForNextFrameFire(duration: CFTimeInterval) {
         frame = frame + 1
         let n = numberOfFrames
         if frame >= n {
-            frame = 0
+            if willRepeat {
+                frame = 0
+            } else {
+                invalid()
+            }
         }
         self.nextDuration += duration
     }
     
+    deinit {
+        print("******hello-------")
+        SwiftDisplayLinkWrapper.shared.removeEventBlock(for: self)
+    }
     
 }
 
@@ -137,9 +135,9 @@ class SwiftDisplayLinkWrapper {
         setup()
     }
     
-    fileprivate static let shared = SwiftDisplayLinkWrapper()
+    static let shared = SwiftDisplayLinkWrapper()
     private var eventBlocks = [ SwiftDisplayLink: SwiftDisplayLinkEventInternalBlock ]()
-    private var isPlaying: Bool = false
+    var isPlaying: Bool = false
         
 #if os(iOS)
     var displaylink: CADisplayLink?
@@ -147,29 +145,18 @@ class SwiftDisplayLinkWrapper {
     var displaylink: CVDisplayLink?
     
     private struct DataHolder {
-        let oldTime: DispatchTime/* = DispatchTime.now()*/
-        let videoTimeScaleOverRefreshPeriod: Double/* = 120*/
-//        var isFirstTimeCalled: Bool = false
-        
-//        mutating func firstTimeCalled(_ scaleTimesRefreshPeriod: Int64) {
-//            videoTimeScaleTimesRefreshPeriod = scaleTimesRefreshPeriod
-//            oldTime = DispatchTime.now()
-////            isFirstTimeCalled = true
-//        }
+        let oldTime: DispatchTime
+        let videoTimeScaleOverRefreshPeriod: Double
     }
     
     private var dataHolderObj = DataHolder(oldTime: DispatchTime.now(),
                                            videoTimeScaleOverRefreshPeriod: 120)
 #endif
     
-//    var eventInternalBlock: SwiftDisplayLinkEventInternalBlock?
 
     func setup() {
 #if os(iOS)
-        displaylink = CADisplayLink(
-            target: self,
-            selector: #selector(SwiftDisplayLinkWrapper.renderFrame(displaylink:))
-        )
+        
 #else
         let displayID = CGMainDisplayID()
         let error = CVDisplayLinkCreateWithCGDisplay(displayID, &displaylink)
@@ -182,39 +169,16 @@ class SwiftDisplayLinkWrapper {
             Swift.print("Error: Failed to get displayLink")
             return
         }
-//        GifAnimatorDisplayLinkHolder.wrapper = self
-        
-
-        
-//        CVDisplayLinkStart(link)
 #endif
     }
     
 #if os(iOS)
-    
-//    var oldTime = DispatchTime.now()
-    
-    @objc func renderFrame(displaylink: CADisplayLink) {
-//        print(displaylink.timestamp, /*displaylink.targetTimestamp,*/ displaylink.duration)
-//        eventInternalBlock?(displaylink.timestamp, /*displaylink.targetTimestamp,*/ displaylink.duration)
-        displayLinkClick(timeStamp: displaylink.timestamp, duration: displaylink.duration)
         
-//        let current = DispatchTime.now()
-//        print("Current Time     ", current.uptimeNanoseconds,
-//              "timeStamp       ", displaylink.timestamp,
-//              "duration", displaylink.duration,
-////              "targetTimestamp", displaylink.targetTimestamp,
-//              "time from Prev", current.uptimeNanoseconds - oldTime.uptimeNanoseconds)
-//        oldTime = current
-
+    @objc func renderFrame(displaylink: CADisplayLink) {
+//        eventInternalBlock?(displaylink.timestamp, displaylink.targetTimestamp, displaylink.duration)
+        displayLinkClick(timeStamp: displaylink.timestamp, duration: displaylink.duration)
     }
 #else
-    
-    
-    
-    
-//    var inNowTime: UInt64 = 0
-//    var inOutputT: UInt64 = 0
     
     func dataSetupCallback( displayLink: CVDisplayLink,
                             inNow: UnsafePointer<CVTimeStamp>,
@@ -245,53 +209,6 @@ class SwiftDisplayLinkWrapper {
         displayLinkContext: Optional<UnsafeMutableRawPointer>
     ) -> Int32 {
         
-        
-        
-//        if inOutputT == 0 {
-//            inOutputT = inNow.pointee.hostTime
-//        }
-//
-//
-//        var timeBaseInfo = mach_timebase_info()
-////        struct mach_timebase_info timeBaseInfo;
-//        mach_timebase_info(&timeBaseInfo);
-//
-//        var clockFrequency = Double(timeBaseInfo.denom) / Double(timeBaseInfo.numer);
-//        clockFrequency = clockFrequency * 100000.0;
-        
-//        let current = DispatchTime.now()
-//        let deltaTime: TimeInterval = 1.0 / (inOutputTime.pointee.rateScalar * Double( inOutputTime.pointee.videoTimeScale) / Double(inOutputTime.pointee.videoRefreshPeriod));
-//        var outTime = CVTimeStamp()
-//        CVDisplayLinkGetCurrentTime(displayLink, &outTime)
-
-//        let timeFromPrevUsingDL = (1.0 / Double(inOutputTime.pointee.hostTime - inNow.pointee.hostTime)) * clockFrequency
-        
-//        print("CurrentTime", current.uptimeNanoseconds,
-////              "inNow Time       ", inNow.pointee.hostTime,
-////              "inOutputTime Time", inOutputTime.pointee.hostTime,
-////              "inOutputTime-inNow Time", inOutputTime.pointee.hostTime - inNow.pointee.hostTime,
-////              "timefromPrev", current.uptimeNanoseconds - oldTime.uptimeNanoseconds,
-////              "inNow from Prev", inNow.pointee.hostTime - inNowTime,
-////              "inOutputTime from Prev", inOutputTime.pointee.hostTime - inOutputT,
-////              "inNowTime - inOutputT", inOutputT - inNow.pointee.hostTime,
-//              "deltaTime", deltaTime,
-////              "outTime", outTime,
-////              "clockFrequency", clockFrequency,
-////              "timeFromPrevUsingDL", timeFromPrevUsingDL,
-//              "rateScalar", inOutputTime.pointee.rateScalar,
-//              "videoTimeScale", inOutputTime.pointee.videoTimeScale,
-//              "videoRefreshPeriod", inOutputTime.pointee.videoRefreshPeriod
-//        )
-
-//        oldTime = current
-//        inNowTime = inNow.pointee.hostTime
-//        inOutputT = inOutputTime.pointee.hostTime
-        
-        
-//        print("inNow", inNow.pointee)
-//        print("inOutputTime", inOutputTime.pointee)
-        
-        
         let deltaTime: TimeInterval = 1.0 / (inOutputTime.pointee.rateScalar * dataHolderObj.videoTimeScaleOverRefreshPeriod);
         let timeStamp = Double(DispatchTime.now().uptimeNanoseconds) * 0.00000000001
         
@@ -302,16 +219,35 @@ class SwiftDisplayLinkWrapper {
 #endif
     
     private func startInternal() {
-        guard let link = displaylink else { return }
 #if os(iOS)
+        let link = CADisplayLink(
+            target: self,
+            selector: #selector(SwiftDisplayLinkWrapper.renderFrame(displaylink:))
+        )
         link.add(to: .main, forMode: .default)
+        displaylink = link
 #else
+        guard let link = displaylink else { return }
 //        dataHolderObj = DataHolder()
         CVDisplayLinkSetOutputCallback(link, { displayLink, inNow, inOutputTime, flagsIn, flagsOut, displayLinkContext in
             SwiftDisplayLinkWrapper.shared.dataSetupCallback(displayLink: displayLink, inNow: inNow, inOutputTime: inOutputTime, flagsIn: flagsIn, flagsOut: flagsOut, displayLinkContext: displayLinkContext)
         }, nil)
         CVDisplayLinkStart(link)
         
+#endif
+    }
+    
+    private func stopInternal() {
+        guard let link = displaylink else { return }
+#if os(iOS)
+//        link.isPaused = true
+        link.invalidate()
+#else
+//        dataHolderObj = DataHolder()
+//        CVDisplayLinkSetOutputCallback(link, { displayLink, inNow, inOutputTime, flagsIn, flagsOut, displayLinkContext in
+//            SwiftDisplayLinkWrapper.shared.dataSetupCallback(displayLink: displayLink, inNow: inNow, inOutputTime: inOutputTime, flagsIn: flagsIn, flagsOut: flagsOut, displayLinkContext: displayLinkContext)
+//        }, nil)
+        CVDisplayLinkStop(link)
 #endif
     }
 }
@@ -345,6 +281,10 @@ extension SwiftDisplayLinkWrapper {
     /// And when the next event will occur on displayLink none of these events will get called.
     func removeEventBlock(for object: SwiftDisplayLink) {
         eventBlocks[object] = nil
+        if eventBlocks.isEmpty {
+            stopInternal()
+            isPlaying = false
+        }
     }
     
     private func displayLinkClick(timeStamp: CFTimeInterval, duration: CFTimeInterval ) {
--- Tests/SwiftDisplayLinkTests/SwiftDisplayLinkTests.swift
+++ Tests/SwiftDisplayLinkTests/SwiftDisplayLinkTests.swift
@@ -3,24 +3,28 @@ import XCTest
 
 final class SwiftDisplayLinkTests: XCTestCase {
     
+    
     func testEventOccuredGettingFired() throws {
-        
         let expectation = expectation(description: "Waitimg for events for each frame to get fired.")
-        
+
         var timeStampsForPerformAction = [DispatchTime]()
         var frameCounter = [Int]()
-        
+
         let displayLink = SwiftDisplayLink(frameCount: 10) { frame in
             SwiftDisplayLinkFrameData(duration: 0.1, isFrameConstructed: true)
         }
-        
+
+        //check why the displayLink has reference count 2 at this place.
+
         //Collecting this time too so that we can find out how much time it takes to fire the first frame.
         timeStampsForPerformAction.append(DispatchTime.now())
-        
+
         displayLink.play { event, frame  in
-            if case .performAction( _, _) = event {
+            if case let .performAction( timestamp, _) = event {
                 timeStampsForPerformAction.append(DispatchTime.now())
                 frameCounter.append(frame)
+                print(frame, timestamp)
+
                 if frame == 9 {
                     expectation.fulfill()
                 }
@@ -29,9 +33,9 @@ final class SwiftDisplayLinkTests: XCTestCase {
                 XCTAssert(false, "The construct frame should not get called as we are passing isFrameConstructed = true in SwiftDisplayLinkFrameData")
             }
         }
-        
+
         waitForExpectations(timeout: 1.0)
-        
+
         let countOfTimeStamps = timeStampsForPerformAction.count
         for index in 1..<countOfTimeStamps-1 {
             let timeIntervalInNanoSecs = timeStampsForPerformAction[index + 1].uptimeNanoseconds - timeStampsForPerformAction[index].uptimeNanoseconds
@@ -40,36 +44,180 @@ final class SwiftDisplayLinkTests: XCTestCase {
             XCTAssert(0.09 < timeInterval && timeInterval < 0.11, "The time interval which is - \(timeInterval) was expected to be around 0.1 (± 0.01)")
         }
     }
-    
-    
+
+
     func testWhenFirstEventIsCalled() throws {
-        
+        XCTAssert( SwiftDisplayLinkWrapper.shared.isPlaying == false )
+
         let expectation = expectation(description: "Waitimg for events for each frame to get fired.")
-        
+
         var timeStampForFirstAction = DispatchTime.now()
-        
+
         let displayLink = SwiftDisplayLink(frameCount: 2) { frame in
             SwiftDisplayLinkFrameData(duration: 0.1, isFrameConstructed: true)
         }
-        
+
         //Collecting this time too so that we can find out how much time it takes to fire the first frame.
 
         let timeStampBeforePlay = DispatchTime.now()
         displayLink.play { event, frame  in
             if case .performAction( _, _) = event {
                 timeStampForFirstAction = DispatchTime.now()
-                expectation.fulfill()
+                if frame == 1 {
+                    expectation.fulfill()
+                }
             }
             if case .constructFrame = event {
                 XCTAssert(false, "The construct frame should not get called as we are passing isFrameConstructed = true in SwiftDisplayLinkFrameData")
             }
         }
-        
+
         waitForExpectations(timeout: 0.2)
-        
+
         let timeIntervalInNanoSecs = timeStampForFirstAction.uptimeNanoseconds - timeStampBeforePlay.uptimeNanoseconds
         //The below code might overflow if the test is too long.
         let timeInterval = Double(timeIntervalInNanoSecs) / 1_000_000_000
-        XCTAssert(0.01 < timeInterval && timeInterval < 0.03, "The time interval which is - \(timeInterval) was expected to be between 0.01 & 0.08") //keeping this well relaxed...
+        XCTAssert(0.01 < timeInterval && timeInterval < 0.2, "The time interval which is - \(timeInterval) was expected to be between 0.01 & 0.2") //keeping this well relaxed...
     }
+    
+    
+    func testFrameConstructionIsAlsoCalled() throws {
+        XCTAssert( SwiftDisplayLinkWrapper.shared.isPlaying == false )
+
+        let expectation = expectation(description: "Waitimg for events for each frame to get fired.")
+
+        let displayLink = SwiftDisplayLink(frameCount: 20) { frame in
+            SwiftDisplayLinkFrameData(duration: 0.05, isFrameConstructed: false)
+        }
+
+        //Collecting this time too so that we can find out how much time it takes to fire the first frame.
+        var arrayIsConstructed:[Int] = [Int]()
+
+        let timeStampBeforePlay = DispatchTime.now()
+        displayLink.play { event, frame  in
+            if case .performAction( _, _) = event {
+                if frame == 19 { expectation.fulfill() }
+                XCTAssert(arrayIsConstructed[frame] == 1)
+                arrayIsConstructed[frame] = 2
+            }
+            
+            if case .constructFrame = event {
+                arrayIsConstructed.append(1)
+            }
+        }
+
+        waitForExpectations(timeout: 1.1)
+        
+        let timeStampForLastAction = DispatchTime.now()
+        let timeIntervalInNanoSecs = timeStampForLastAction.uptimeNanoseconds - timeStampBeforePlay.uptimeNanoseconds
+        let timeInterval = Double(timeIntervalInNanoSecs) / 1_000_000_000
+        XCTAssert(timeInterval < 1.01 && timeInterval > 0.9)
+        
+        XCTAssert(arrayIsConstructed.count == 20)
+        XCTAssert(arrayIsConstructed.filter({ i in i == 2 }).count == 20 )
+    }
+
+    func testmultipleLinks() throws {
+        
+        var counter = 0
+        func runDisplaylink() throws {
+            let expectation = expectation(description: "Waitimg for events for each frame to get fired.")
+            let displayLink = SwiftDisplayLink(frameCount: 20) { frame in
+                SwiftDisplayLinkFrameData(duration: 0.05, isFrameConstructed: true)
+            }
+            displayLink.play { event, frame  in
+                if case .performAction( _, _) = event {
+                    counter = counter + 1
+                    if frame == 19 { expectation.fulfill() }
+                }
+            }
+        }
+        
+        XCTAssert( SwiftDisplayLinkWrapper.shared.isPlaying == false )
+        
+        let timeStampBeforePlay = DispatchTime.now()
+        for _ in 0..<100 {
+            try runDisplaylink()
+        }
+        waitForExpectations(timeout: 1.1)
+        
+        let timeStampForLastAction = DispatchTime.now()
+        let timeIntervalInNanoSecs = timeStampForLastAction.uptimeNanoseconds - timeStampBeforePlay.uptimeNanoseconds
+        let timeInterval = Double(timeIntervalInNanoSecs) / 1_000_000_000
+        XCTAssert(timeInterval < 1.01 && timeInterval > 0.9)
+        XCTAssert(counter == 2000)
+    }
+    
+    func testPausePlay() throws {
+        let expect = expectation(description: "Waitimg for events for each frame to get fired.")
+        var expect2: XCTestExpectation? = nil
+
+        var timeStampsForPerformAction = [DispatchTime]()
+        var frameCounter = [Int]()
+
+        let displayLink = SwiftDisplayLink(frameCount: 20) { frame in
+            SwiftDisplayLinkFrameData(duration: 0.1, isFrameConstructed: true)
+        }
+
+        //check why the displayLink has reference count 2 at this place.
+
+        //Collecting this time too so that we can find out how much time it takes to fire the first frame.
+        timeStampsForPerformAction.append(DispatchTime.now())
+
+        displayLink.play { event, frame  in
+            if case let .performAction( timestamp, _) = event {
+                timeStampsForPerformAction.append(DispatchTime.now())
+                frameCounter.append(frame)
+                print(frame, timestamp)
+                if frame == 9 {
+                    displayLink.pause()
+                    expect.fulfill()
+                }
+                if frame == 19 {
+                    displayLink.pause()
+                    expect2?.fulfill()
+                }
+            }
+        }
+
+        waitForExpectations(timeout: 1.1)
+        displayLink.resume()
+        
+        expect2 = expectation(description: "Waitimg for events for each frame to get fired.")
+        waitForExpectations(timeout: 1.1)
+
+        let countOfTimeStamps = timeStampsForPerformAction.count
+        for index in 1..<countOfTimeStamps-1 {
+            let timeIntervalInNanoSecs = timeStampsForPerformAction[index + 1].uptimeNanoseconds - timeStampsForPerformAction[index].uptimeNanoseconds
+            //The below code might overflow if the test is too long.
+            let timeInterval = Double(timeIntervalInNanoSecs) / 1_000_000_000
+            XCTAssert(0.09 < timeInterval && timeInterval < 0.11, "The time interval which is - \(timeInterval) was expected to be around 0.1 (± 0.01)")
+        }
+        
+    }
+    
+    func testrepeatTimer() throws {
+        let expect = expectation(description: "Waitimg for events for each frame to get fired.")
+
+        var frameCounter = [Int]()
+        let displayLink = SwiftDisplayLink(frameCount: 5, repeatFrames: true) { frame in
+            SwiftDisplayLinkFrameData(duration: 0.02, isFrameConstructed: true)
+        }
+
+        displayLink.play { event, frame  in
+            if case .performAction( _, _) = event {
+                frameCounter.append(frame)
+                if frameCounter.count == 100 {
+                    displayLink.invalid()
+                    expect.fulfill()
+                }
+            }
+        }
+
+        waitForExpectations(timeout: 2.01)
+        
+        XCTAssert(frameCounter.count == 100)        
+    }
+    
 }
+
